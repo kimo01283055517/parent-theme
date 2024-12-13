503dea2f6517b662661bd838702a1359ea33c271
Kimo01283055517 <alnazrs641@gmail.com>
2024-12-13 16:30:08 +0200

    adjust image with manual image crop extaintion

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php 
     function add_style(){
-        wp_enqueue_script( "style", get_theme_file_uri( "style.css" ) );
+        wp_enqueue_style( "style", get_theme_file_uri( "style.css" ) );
     }
 
     add_action( 'wp_enqueue_scripts','add_style');
-
-
+    add_theme_support( 'post-thumbnails' );
+    add_image_size( 'custom-size', 300, 200, true ); 
 
 ?>
--- single-boy.php
+++ single-boy.php
@@ -6,12 +6,15 @@ get_header();
 <h1><?php echo get_the_title(); ?></h1>
 <h1><?php echo get_the_title(get_field('parent')[0]); ?></h1>
 <h1><?php echo get_the_ID(); ?></h1>
+<img class="image" src="<?php the_post_thumbnail_url( 'custom-size' )?>" alt="">
+
+<img class="image" src="<?php echo get_field('background')['sizes']['custom-size']?>" alt="">
 
 <h1></h1>
 <h1><a href="<?php echo get_post_type_archive_link(get_post_type(  )) ; ?>"><?php echo get_post_type(  ) ; ?></a></h1>
 
 <?php
-var_dump(get_field('parent')[0]);
+var_dump(get_field('background'));
 get_footer(  );
 
 ?>
