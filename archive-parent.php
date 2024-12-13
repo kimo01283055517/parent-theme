4e5b49ac013d701b85a465972732670d06600813
Kimo01283055517 <alnazrs641@gmail.com>
2024-12-13 11:23:08 +0200

    fix some

---
+++ archive-parent.php
@@ -0,0 +1,34 @@
+<?php
+get_header();
+while(have_posts(  )):the_post();
+?>
+
+
+</h1>
+<h1><?php echo get_the_ID(); ?></h1>
+<h1><?php echo get_the_title(  ); ?>---</h1>
+<h1><?php echo get_the_archive_title(); ?></h1>
+
+<?php
+
+$arr = array('post_type'=>'boy',
+"meta_query"=>
+array(array(
+    'key'=>'parent',
+    'compare'=>'LIKE',
+    'value'=>'"'.get_the_ID().'"'
+)));
+$qu =new WP_Query($arr);
+
+while($qu->have_posts()):$qu->the_post();
+ 
+?>
+<h1><a href="<?PHP the_permalink(  )?>">
+<?php echo get_the_title(  ); ?>
+</a></h1>
+<?PHP
+  endwhile;  wp_reset_postdata(  );
+endwhile;wp_reset_postdata(  );
+get_footer(  );
+
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 get_header();
 
 ?>
+<h1> in the name of allah</h1>
 <h1><?php echo get_the_ID(); ?></h1>
 <h1><?php echo get_the_title(); ?></h1>
 <h1><?php echo get_the_archive_title(); ?></h1>
